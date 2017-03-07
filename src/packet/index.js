dcb05d722fed8044dd4d13836d1f12704ac98a52
ZfsrGhS953 <j1355444@trbvm.com>
2017-03-07 23:55:30 +0300

    Create index.js

---
+++ src/packet/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+	AddNode: require("./AddNode"),
+	ClearAll: require("./ClearAll"),
+	LeaderboardPosition: require("./LeaderboardPosition"),
+	SetBorder: require("./SetBorder"),
+	UpdateLeaderboard: require("./UpdateLeaderboard"),
+	UpdateNodes: require("./UpdateNodes"),
+	UpdatePosition: require("./UpdatePosition"),
+	Writer: require("./Writer"),
+};
